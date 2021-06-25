0b22f3be99c4900ef850a4e5a97f29db0f65511d
Vlad George-Alexandru <vlad.alexandru@soft-tehnica.com>
2021-06-25 15:47:56 +0300

    Added intents about AI/ML/DL
    Support for adding links to specific intent
    Support for remembering conversation state for asking additional
    questions on the subject
    
    -----
    
    ```
    Bot loaded. Type a message and press enter (use '/stop' to exit):
    Your input ->  salut
    Salut!
    Your input ->  ce este ai?
    În informatică, inteligența artificială (IA) este inteligența expusă de
    mașini, spre deosebire de inteligența naturală, expusă de oameni și de
    unele animale. Kaplan și Haenlein definesc IA ca fiind „capacitatea unui
    sistem de a interpreta corect datele externe, de a învăța din astfel de
    date și de a folosi ceea ce a învățat pentru a-și atinge obiective și
    sarcini specifice printr-o adaptare flexibilă”.
    Your input ->  alta definitie
    Informatica definește cercetarea IA ca studiu al „agenților
    inteligenți”: orice dispozitiv care își percepe mediul și efectuează
    acțiuni care maximizează șansa de a-și atinge cu succes obiectivele.
    Your input ->  definitie alternativa
    Termenul „inteligență artificială” este utilizat colocvial pentru a
    descrie mașinile care imită funcțiile „cognitive” pe care le asociază
    oamenii cu alte minți umane, cum ar fi „învățarea” și „rezolvarea
    problemelor”.
    Your input ->  alta def
    Am epuizat informatiile despre aceasta tema. Continui cautarea pe
    google.compentru Artificial Intelligence definition.
    Am gasit urmatoarele rezultate care te-ar putea ajuta:
    https://en.wikipedia.org/wiki/Artificial_intelligence
    https://en.wikipedia.org/wiki/Glossary_of_artificial_intelligence
    https://en.wikipedia.org/wiki/Artificial_general_intelligence
    https://en.wikipedia.org/wiki/Symbolic_artificial_intelligence
    https://en.wikipedia.org/wiki/History_of_artificial_intelligence
    Your input ->  da-mi alta definitie
    Despre ce subiect ai dori sa afli mai multe? :)
    Your input ->  despre dl
    Învățarea profundă (în engleză, „Deep Learning”) este o ramura a
    inteligenței artificiale (IA) care imită funcționarea creierului uman în
    procesarea datelor și crearea de modele pentru luarea deciziilor. Deep
    Learning foloseste retele neurale pentru a invata in regim
    supervizat/nesupervizat din date nestructurate sau nemarcate.
    Your input ->  alta definitie
    Am epuizat informatiile despre aceasta tema. Continui cautarea pe
    google.compentru Deep learning definition.
    Am gasit urmatoarele rezultate care te-ar putea ajuta:
    https://en.wikipedia.org/wiki/Deep_learning
    https://en.wikipedia.org/wiki/Feature_learning
    https://en.wikipedia.org/wiki/Deep_belief_network
    https://en.wikipedia.org/wiki/Semi-supervised_learning
    https://machinelearningmastery.com/what-is-deep-learning/
    Your input ->  da-mi un link pentru instalat python
    Uite cateva tutoriale bune pentru python:
            1. https://docs.python.org/3/tutorial/
    	        2. https://docs.python.org/3/tutorial/interpreter.html
    		S-a rezolvat?
    		Your input ->  da
    		Super, ma bucur!
    		Your input ->
    ```
    
    Signed-off-by: Vlad George-Alexandru <vlad.alexandru@soft-tehnica.com>

--- actions/actions.py
+++ actions/actions.py
@@ -8,9 +8,9 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, FollowupAction
-
+from rasa_sdk.events import SlotSet
 from googlesearch import search
+from functools import reduce
 
 
 class ActionEnvConfig(Action):
@@ -23,7 +23,6 @@ class ActionEnvConfig(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
         latest_message = tracker.latest_message
         entities = latest_message['entities']
         slots = tracker.slots.keys()
@@ -70,3 +69,67 @@ class ActionEnvConfig(Action):
             resulting_links += '\t{}. {}\n'.format(idx + 1, link)
 
         return resulting_links
+
+
+class ActionRespondAIQuestions(Action):
+    QUERIES = {
+        "learn_ai/ask_ai": "Artificial Intelligence definition",
+        "learn_ai/ask_dl": "Deep learning definition",
+        "learn_ai/ask_ml": "Machine learning definition",
+    }
+
+    def name(self) -> Text:
+        return "action_respond_ai_questions"
+
+    def __init__(self):
+        self.counter = 0
+        self.state = None
+        self.mem_responses = None
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        intent = tracker.latest_message["intent"]["name"]
+        response_selector = tracker.latest_message.get("response_selector", None)
+        response = response_selector["default"]["response"]
+        intent_response_key = response["intent_response_key"]
+        responses = response["responses"]
+        print(tracker.latest_message)
+        print()
+
+        if intent == "ask_for_more":
+            if self.state is None:
+                dispatcher.utter_message(text="Despre ce subiect ai dori sa afli mai multe? :)")
+                return []
+
+            # we should check if in ask_for_more intent we have a specific entity (ml, dl, ai)
+            # if we don't, it means we are referring to the previous one
+            # sometimes the bot can get the answers wrong, so we'd better retrieve them from a buffer
+            if self.counter < (len(self.mem_responses) - 1):
+                self.counter += 1
+            else:
+                query = self.QUERIES[self.state]
+                dispatcher.utter_message("Am epuizat informatiile despre aceasta tema. Continui cautarea pe google.com"
+                                         "pentru {}.".format(query))
+                links = list(search(query, lang="en", num=5, stop=5, pause=0.5))
+                response = "Am gasit urmatoarele rezultate care te-ar putea ajuta:\n"
+                response += reduce(lambda a, b: a + "\n" + b, links)
+
+                dispatcher.utter_message(text=response)
+
+                self.state = None
+                self.counter = 0
+                return []
+        else:
+            if intent_response_key != self.state:
+                self.mem_responses = responses
+                self.state = intent_response_key
+                self.counter = 0
+            else:
+                self.counter += 1
+
+        response = self.mem_responses[self.counter]["text"]
+        dispatcher.utter_message(text=response)
+
+        return []
