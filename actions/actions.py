24f8bad107a8d253929b2350214c2ffca366ece7
Bercanu Gabriel <gabriel.bercanu@soft-tehnica.local>
2021-06-25 14:30:48 +0300

    intents: added 'env_config' intent
    Added a handling action for intents related to work environment
    configuration steps, such as installing a programming language
    or a framework.
    
    This commits introduces 3 new entities:
      * programming_language
      * package
      * domain
    
    When required, the chatbot performs an action which extracts
    entities from the user query (only `programming_language` and
    `package` for now) and retrieves appropriate links related to
    the entities supplied.
    
    ----------
    
    Testing done:
    
    Testing has only been performed within interactive `rasa shell`
    sessions:
    
    ```
    Bot loaded. Type a message and press enter (use '/stop' to exit):
    Your input ->  salut
    Ce mai zici?
    Your input ->  am nevoie de niste tutoriale de python si tensorflow
    Uite cateva tutoriale bune pentru python:
            1. https://docs.python.org/3/tutorial/
    	2. https://docs.python.org/3/tutorial/interpreter.html
    La link-urile de aici am gasit niste informatii despre tensorflow:
    	1. https://www.tensorflow.org/tutorials
    	2. https://www.tensorflow.org/tutorials/quickstart/beginner
    Ai reusit?
    Your input ->  da
    Super, ma bucur!
    Your input ->  mersi, pa
    Pa
    Your input ->  /stop
    ```
    
    Signed-off-by: Bercanu Gabriel <gabriel.bercanu@soft-tehnica.local>

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,69 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, FollowupAction
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from googlesearch import search
+
+
+class ActionEnvConfig(Action):
+    REQUEST_FREQUENCY = 0.5
+    NUM_RETRIEVED_LINKS = 2
+
+    def name(self) -> Text:
+        return "action_env_config"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        latest_message = tracker.latest_message
+        entities = latest_message['entities']
+        slots = tracker.slots.keys()
+
+        has_programming_language = False
+        has_package = False
+        has_domain = False
+
+        entities_plus_values = []
+
+        result = []
+        for el in entities:
+            if el['entity'] == 'programming_language':
+                has_programming_language = True
+            if el['entity'] == 'package':
+                has_package = True
+            if el['entity'] == 'domain':
+                has_domain = True
+
+            entities_plus_values.append((el['entity'], el['value']))
+
+            if el['entity'] in slots:
+                result.append(SlotSet(el['entity'], el['value']))
+
+        for entity_, value_ in entities_plus_values:
+            links = self._get_links_for_entity(value_)
+            if entity_ == 'package':
+                dispatcher.utter_message(text="La link-urile de aici am gasit niste informatii despre {}: \n{}"
+                                         .format(value_, links))
+            if entity_ == 'programming_language':
+                dispatcher.utter_message(text="Uite cateva tutoriale bune pentru {}: \n{}"
+                                         .format(value_, links))
+
+        return result
+
+    @staticmethod
+    def _get_links_for_entity(entity_value):
+        resulting_links = ''
+
+        query = entity_value + ' tutorial'
+        for idx, link in enumerate(search(query, tld="com", num=ActionEnvConfig.NUM_RETRIEVED_LINKS,
+                                          stop=ActionEnvConfig.NUM_RETRIEVED_LINKS,
+                                          pause=ActionEnvConfig.REQUEST_FREQUENCY)):
+            resulting_links += '\t{}. {}\n'.format(idx + 1, link)
+
+        return resulting_links
