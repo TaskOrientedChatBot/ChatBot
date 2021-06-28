f832a67d25060456e580e1e75092e8fc41535eb8
Bercanu Gabriel <gabriel.bercanu@soft-tehnica.local>
2021-06-28 11:16:36 +0300

    intents: split `env_config` intent
    Separate the `env_config` intent from the `domain_general_knowledge`
    intent. The latter contains less examples and is only restricted
    for usecases where `package` or `programming_language` entities
    are used together with `domain` entities (such as 'AI' / 'ML' / 'DL').
    
    -----
    
    Testing done: the agent has been tested by manual validation, through
    `rasa shell`:
    
    ```
    Bot loaded. Type a message and press enter (use '/stop' to exit):
    Your input ->  salut
    Salut! Sunt asistentul tau si te voi ajuta sa inveti sa dezvolti primul
    program in inteligenta artificiala.
    Your input ->  super, imi poti da niste info generale despre domeniul de
    AI?
    În informatică, inteligența artificială (IA) este inteligența expusă de
    mașini, spre deosebire de inteligența naturală, expusă de oameni și de
    unele animale. Kaplan și Haenlein definesc IA ca fiind „capacitatea unui
    sistem de a interpreta corect datele externe, de a învăța din astfel de
    date și de a folosi ceea ce a învățat pentru a-și atinge obiective și
    sarcini specifice printr-o adaptare flexibilă”.
    Your input ->  ok, cam cat dureaza sa invat AI ?
    Am pregatit ceva care te-ar putea inveseli:
    Image: https://i.imgur.com/nGF1K8f.jpg
    Ai reusit?
    Your input ->  cam cat dureaza sa invat AI ?
    Uite ceva sa te inveseleasca:
    Image: https://postimg.cc/Y4dMNHkF
    S-a rezolvat?
    Your input ->  cam cat dureaza sa invat ML?
    Daca esti interesat de domeniul de ML, poti incepe sa citesti de aici:
            1.https://en.wikipedia.org/wiki/Machine_learning
    	2.https://searchenterpriseai.techtarget.com/definition/machine-learning-ML
    Your input ->  ok super, mersi
    Super!
    Your input ->  bye
    Salut!
    Your input -> /stop
    ```
    
    Signed-off-by: Bercanu Gabriel <gabriel.bercanu@soft-tehnica.local>

--- actions/actions.py
+++ actions/actions.py
@@ -9,13 +9,27 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-from googlesearch import search
+
 from functools import reduce
+from googlesearch import search
+from random import randrange
+
 
 
 class ActionEnvConfig(Action):
     REQUEST_FREQUENCY = 0.5
     NUM_RETRIEVED_LINKS = 2
+    POSSIBLE_PACKAGE_RESPONSES = ["La link-urile de aici am gasit niste informatii despre {}: \n{}",
+                                  "Cateva link-uri despre {}: \n{}",
+                                  "Am gasit cate ceva despre {}: \n{}",
+                                  "Uite cateva link-uri interesante despre configurat {}: \n{}",
+                                  "Daca esti interesat de {}, poti incepe cu link-urile de aici: \n{}"]
+    POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES = ["Uite cateva tutoriale bune pentru {}: \n{}",
+                                               "Ca sa inveti {}, poti incepe cu link-urile acestea: \n{}",
+                                               "Link-urile de aici ar trebui sa contina suficiente informatii despre"
+                                               "\n{}",
+                                               "Poti incepe sa inveti despre {} cu link-urile de mai jos: \n{}",
+                                               "Link-urile de aici ar trebui sa fie de folos pentru invatat {}: \n{}"]
 
     def name(self) -> Text:
         return "action_env_config"
@@ -50,23 +64,97 @@ class ActionEnvConfig(Action):
         for entity_, value_ in entities_plus_values:
             links = self._get_links_for_entity(value_)
             if entity_ == 'package':
-                dispatcher.utter_message(text="La link-urile de aici am gasit niste informatii despre {}: \n{}"
+                rand_response_idx = randrange(len(ActionEnvConfig.POSSIBLE_PACKAGE_RESPONSES))
+                dispatcher.utter_message(text=ActionEnvConfig.POSSIBLE_PACKAGE_RESPONSES[rand_response_idx]
                                          .format(value_, links))
             if entity_ == 'programming_language':
-                dispatcher.utter_message(text="Uite cateva tutoriale bune pentru {}: \n{}"
+                rand_response_idx = randrange(len(ActionEnvConfig.POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES))
+                dispatcher.utter_message(text=ActionEnvConfig.POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES[rand_response_idx]
                                          .format(value_, links))
 
         return result
 
     @staticmethod
     def _get_links_for_entity(entity_value):
+        info_query = '{} tutorial'
         resulting_links = ''
 
-        query = entity_value + ' tutorial'
-        for idx, link in enumerate(search(query, tld="com", num=ActionEnvConfig.NUM_RETRIEVED_LINKS,
-                                          stop=ActionEnvConfig.NUM_RETRIEVED_LINKS,
-                                          pause=ActionEnvConfig.REQUEST_FREQUENCY)):
-            resulting_links += '\t{}. {}\n'.format(idx + 1, link)
+        query = info_query.format(entity_value)
+        links = list(search(query, tld="com", num=ActionEnvConfig.NUM_RETRIEVED_LINKS,
+                            stop=ActionEnvConfig.NUM_RETRIEVED_LINKS,
+                            pause=ActionEnvConfig.REQUEST_FREQUENCY))
+        links_plus_idx = list(zip(links, list(range(1, len(links) + 1))))
+        links = list(map(lambda x: '\t' + str(x[1]) + '.' + x[0], links_plus_idx))
+        resulting_links += reduce(lambda a, b: a + '\n' + b, links)
+
+        return resulting_links
+
+
+class ActionDomainGeneralKnowledge(Action):
+    REQUEST_FREQUENCY = 0.5
+    NUM_RETRIEVED_LINKS = 2
+    POSSIBLE_PACKAGE_RESPONSES = ["La link-urile de aici am gasit niste informatii despre {}: \n{}",
+                                  "Cateva informatii generale despre {}: \n{}",
+                                  "Cateva lucruri despre {}: \n{}",
+                                  "Uite cateva link-uri despre {}: \n{}",
+                                  "Informatii de baza despre {} gasesti aici: \n{}"]
+    POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES = ["Uite cateva tutoriale bune pentru {}: \n{}",
+                                               "Informatii generale despre {}: \n{}",
+                                               "Poti sa citesti aici despre {}, pentru inceput: \n{}",
+                                               "Poti incepe sa inveti despre {} cu link-urile de mai jos: \n{}"]
+    POSSIBLE_DOMAIN_RESPONSES = ["Aici am gasit niste informatii de baza despre {}: \n{}",
+                                 "Cateva informatii generale despre domeniul de {}: \n{}",
+                                 "Daca esti interesat de domeniul de {}, poti incepe sa citesti de aici: \n{}"]
+
+    def name(self) -> Text:
+        return "action_domain_general_knowledge"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        latest_message = tracker.latest_message
+        entities = latest_message['entities']
+        slots = tracker.slots.keys()
+
+        entities_plus_values = []
+
+        result = []
+        for el in entities:
+            entities_plus_values.append((el['entity'], el['value']))
+
+            if el['entity'] in slots:
+                result.append(SlotSet(el['entity'], el['value']))
+
+        for entity_, value_ in entities_plus_values:
+            links = self._get_links_for_entity(value_)
+            if entity_ == 'package':
+                rand_response_idx = randrange(len(ActionDomainGeneralKnowledge.POSSIBLE_PACKAGE_RESPONSES))
+                dispatcher.utter_message(text=ActionDomainGeneralKnowledge.POSSIBLE_PACKAGE_RESPONSES[rand_response_idx]
+                                         .format(value_, links))
+            if entity_ == 'programming_language':
+                rand_response_idx = randrange(len(ActionDomainGeneralKnowledge.POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES))
+                dispatcher.utter_message(text=ActionDomainGeneralKnowledge
+                                         .POSSIBLE_PROGRAMMING_LANGUAGE_RESPONSES[rand_response_idx]
+                                         .format(value_, links))
+            if entity_ == 'domain':
+                rand_response_idx = randrange(len(ActionDomainGeneralKnowledge.POSSIBLE_DOMAIN_RESPONSES))
+                dispatcher.utter_message(text=ActionDomainGeneralKnowledge.POSSIBLE_DOMAIN_RESPONSES[rand_response_idx]
+                                         .format(value_, links))
+
+        return result
+
+    @staticmethod
+    def _get_links_for_entity(entity_value):
+        info_query = '{} general information'
+        resulting_links = ''
+
+        query = info_query.format(entity_value)
+        links = list(search(query, tld="com", num=ActionDomainGeneralKnowledge.NUM_RETRIEVED_LINKS,
+                            stop=ActionDomainGeneralKnowledge.NUM_RETRIEVED_LINKS,
+                            pause=ActionDomainGeneralKnowledge.REQUEST_FREQUENCY))
+        links_plus_idx = list(zip(links, list(range(1, len(links) + 1))))
+        links = list(map(lambda x: '\t' + str(x[1]) + '.' + x[0], links_plus_idx))
+        resulting_links += reduce(lambda a, b: a + '\n' + b, links)
 
         return resulting_links
 
